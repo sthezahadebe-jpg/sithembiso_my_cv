45af90212b87a61e092a64bbbfd9efee5574dec8
sthezahadebe-jpg <ssithembisosifiso@gmail.com>
2022-11-29 14:59:56 +0200

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
-      <h1 className="text-4xl font-bold">Hello world!</h1>
+      <NavBar />
+      <Home />
     </div>
       
     
---
+++ src/components/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+// import Sithembiso from '../assets/sithembiso.jpeg';
+// import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <h2>
+                        I'm  a Full Stack Developer
+                    </h2>
+                    <p>
+                        I am 33 year old. I live in Newcastle place madadeni section 5, I students Edeaf 1 year 2015 and PMI 1 year
+                        2016 at college in johannesburg. I am currently a students at the digital academy
+                    </p>
+                    <div>
+                        <button>
+                            Portfolio
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home;
+
+
+// <MdOutlineKeyboardArrowRight />
+// {/* <img src={Sithembiso} alt="" /> */}
